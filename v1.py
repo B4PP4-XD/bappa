973050d1305e87b4fe3474f391e94a2cd12d14f8
Bappa debnath <146751024+bappa005@users.noreply.github.com>
2024-01-09 17:48:49 +0600

    new.py

--- v1.py
+++ v1.py
@@ -234,7 +234,7 @@ def naim2(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb = {'authority': 'free.facebook.com',
+            header_freefb = {'authority': 'p.facebook.com',
           'method':'POST',
            'path':'/?tbua=1',
            'scheme':'https',          
